6c1cf9b54c22343ac5a5f16d1240ac12831877bc
Necdet <117301669+AhplaXx@users.noreply.github.com>
2024-08-15 21:31:33 +0300

    New Model Added and UTF-8 Fixed

--- lib/country_list/models/country_list_models.dart
+++ lib/country_list/models/country_list_models.dart
@@ -2,12 +2,14 @@ import 'dart:convert';
 
 class Country {
   final String name;
+  final String capital;
 
-  Country({required this.name});
+  Country({required this.name, required this.capital});
 
   factory Country.fromJson(Map<String, dynamic> json) {
     return Country(
-      name: json['name']['common'] as String,
+      name: Utf8Decoder().convert(json['name']['common'].toString().codeUnits) ,
+      capital: (json['capital'] != null ) ? Utf8Decoder().convert(json['capital'][0].toString().codeUnits) : 'No Capital'
     );
   }
 
