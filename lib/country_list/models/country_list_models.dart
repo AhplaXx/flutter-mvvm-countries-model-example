7c5d567e3f72e288c3df9f62f2ea11cf95c55c31
Necdet <117301669+AhplaXx@users.noreply.github.com>
2024-08-18 00:41:19 +0300

    independent data added

--- lib/country_list/models/country_list_models.dart
+++ lib/country_list/models/country_list_models.dart
@@ -4,14 +4,19 @@ class Country {
   final String name;
   final String capital;
   final String flag_url;
+  final bool independent;
 
-  Country({required this.name, required this.capital,required this.flag_url});
+
+
+  Country({required this.name, required this.capital,required this.flag_url,required this.independent});
 
   factory Country.fromJson(Map<String, dynamic> json) {
     return Country(
       name: const Utf8Decoder().convert(json['name']['common'].toString().codeUnits) ,
       capital: (json['capital'] != null ) ? const Utf8Decoder().convert(json['capital'][0].toString().codeUnits) : 'No Capital',
-      flag_url: json['flags']['png'] as String
+      flag_url: json['flags']['png'] as String,
+      independent: json['independent'] as bool? ?? false
+
     );
   }
 
