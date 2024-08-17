aa6a3ccbce0a0b4e6edb5c181ddec8bbfbf1a3c8
Necdet <117301669+AhplaXx@users.noreply.github.com>
2024-08-17 17:40:11 +0300

    new views added

---
+++ lib/components/countrylist.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class Userlist extends StatelessWidget {
+  const Userlist({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      child: Container(
+
+      ),
+    );
+  }
+}
