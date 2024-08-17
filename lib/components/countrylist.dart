d1bd8c9db701ba8a278c0e5d49e69dd0e819b6ca
Necdet <117301669+AhplaXx@users.noreply.github.com>
2024-08-17 17:49:19 +0300

    new countrylist component added

--- lib/components/countrylist.dart
+++ lib/components/countrylist.dart
@@ -1,14 +1,27 @@
+import 'package:country/country_list/models/country_list_models.dart';
+import 'package:country/utils/navigation.dart';
 import 'package:flutter/material.dart';
 
 
-class Userlist extends StatelessWidget {
-  const Userlist({super.key});
+class Countrylist extends StatelessWidget {
+  final Country country;
+  final VoidCallback onTap;
+  Countrylist({super.key, required this.country, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
+
+
+
     return InkWell(
+      onTap: onTap,
       child: Container(
 
+        child: Column(
+          children: [
+            Text(country.name + " --- "+country.capital,style: TextStyle(color: Colors.black),)
+          ],
+        ),
       ),
     );
   }
